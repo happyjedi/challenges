263e684ca57ddf6178589274a7b69e9a982b018e
Victor Vinogradov <exploit.vn@yandex.ru>
2017-08-17 16:47:58 +0300

    Add Codility, Lesson 2 Cyclic rotation

---
+++ spec/codility/lesson2-2-cyclic_rotation_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative '../../Codility/lesson2-2-cyclic_rotation'
+
+describe 'solution' do
+  context "when produced 1 rotation" do
+    it "returns one rotated array" do
+      expect(solution([3, 8, 9, 7, 6], 1)).to eql([6, 3, 8, 9, 7])
+    end
+  end
+
+  context "when given an empty array" do
+    it "returns the empty array" do
+      expect(solution([], 10)).to eql([])
+    end
+  end
+end
